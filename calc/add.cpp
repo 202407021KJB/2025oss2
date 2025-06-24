474e4910e48a900792d4011eb76b9af3685b27a1
202407021KJB <jongbum1111@yuhan.ac.kr>
2025-06-24 16:13:54 +0900

    2. add.cpp  [ + ] 병합 충돌 오류로 main에서 add.cpp 변경함.

--- calc/add.cpp
+++ calc/add.cpp
@@ -1,9 +1,8 @@
 #include "add.h"
 #include <iostream>
 
-// add function modify for test
 int add(int x, int y)
 {
-	printf("x[ %d ]�� y[ %d ]�� ���� ���մϴ�.\n");
+	printf("x[ %d ]와 y[ %d ]값을 더합니다.\n");
 	return x + y;
 }
